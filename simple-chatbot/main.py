af531d9acde1bf18e8643b1be70b8b9d71022583
noor ul ain ahmed <123322449+NoorAhmed786@users.noreply.github.com>
2025-04-14 07:35:19 +0500

    simple-chatbot

---
+++ simple-chatbot/main.py
@@ -0,0 +1,9 @@
+import chainlit as cl # type: ignore
+
+@cl.on_message
+async def main(message: cl.Message):
+
+    response= f"You said: {message.content}"
+    await cl.Message(content=response).send() # 
+
+
